57b8ceba80f4cb316000d43d8d46ee3e007a5a97
Celaleddin Hidayetoğlu <celaleddinhidayetoglu@gmail.com>
2019-03-17 17:27:00 +0300

    Add license and fix setup.py packages problem

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup
 
-
 with open('README.md', 'r') as readme:
     long_description = readme.read()
 
 setup(
     name='gently',
-    version='0.1',
+    version='0.11',
     description='A tool for designing and analysing control systems',
     long_description=long_description,
     author='Celaleddin Hidayetoğlu',
     author_email='celaleddin.hidayetoglu@gmail.com',
     url='https://github.com/celaleddin/gently/',
+    license='MIT',
     packages=['gently'],
-    scripts=['scripts/gently', 'scripts/gently.bat'],
+    package_data={
+        'gently': ['*.hy'],
+    },
     install_requires=['hy', 'sympy', 'control', 'matplotlib'],
+    scripts=['scripts/gently', 'scripts/gently.bat'],
 )
